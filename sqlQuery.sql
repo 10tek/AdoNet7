87bf626ffcc690fe729b767213dc7541893c772b
10tek <52250820+10tek@users.noreply.github.com>
2019-10-28 03:15:08 +0600

    Скрипт для заполнения таблиц

---
+++ sqlQuery.sql
@@ -0,0 +1,21 @@
+Insert into Users Values
+('d4659e0a-240d-4151-98eb-569257ff3a85', 'Nagibator228'),
+('7682e724-dcb0-46f5-8b6d-8da867dfa6d1', 'dedede'),
+('84bef081-49fe-44b7-9aec-d24180e25c89', 'qwqwqw'),
+('b8186ff0-386e-41be-908b-a6e5a542648b', 'ssssss'),
+('acc4ff83-ce95-4bd7-989a-f10cecb7a7ef', 'oooo');
+
+Insert into VideoGames Values
+('191f0af5-31a0-4a6e-ba47-7b216ba653d8', 'Игра для игры', 'Igra: Game', 0),
+('d09c06d7-68b0-426d-aea7-2f36ca409523', 'Игра не для игры', 'Game: no game', 0),
+('d1df9968-a2b5-4f9c-9500-100e56ae1dcb', 'Играешь для игры в игру, в которой есть игра', 'Game gaming in the Game(GGitG)', 0),
+('88c321d1-f1c4-43a7-a446-b7618c02456a', 'МОБА игра для задротов', 'LOL', 0);
+
+Insert into Ratings Values
+('32bdc8c2-8f1b-40dc-85ac-9a3734fa83dc', 'd4659e0a-240d-4151-98eb-569257ff3a85', '191f0af5-31a0-4a6e-ba47-7b216ba653d8', 5),
+('8439bcd0-2ffd-4889-9c1f-79153e08daf4', 'd4659e0a-240d-4151-98eb-569257ff3a85', 'd09c06d7-68b0-426d-aea7-2f36ca409523', 3),
+('7889e9da-6120-429a-a119-e5df0c290150', '7682e724-dcb0-46f5-8b6d-8da867dfa6d1', 'd1df9968-a2b5-4f9c-9500-100e56ae1dcb', 2),
+('d1446be8-8403-47d0-849f-427f19ca0e5f', '84bef081-49fe-44b7-9aec-d24180e25c89', '88c321d1-f1c4-43a7-a446-b7618c02456a', 1),
+('584bca0c-3d55-4286-b51e-f2c834e3f458', '84bef081-49fe-44b7-9aec-d24180e25c89', '88c321d1-f1c4-43a7-a446-b7618c02456a', 4),
+('fc91b77d-c7c3-4f7c-a719-36f3bb60347b', 'b8186ff0-386e-41be-908b-a6e5a542648b', 'd1df9968-a2b5-4f9c-9500-100e56ae1dcb', 4),
+('59add580-4844-40e5-a97f-c172523688bf', 'acc4ff83-ce95-4bd7-989a-f10cecb7a7ef', '191f0af5-31a0-4a6e-ba47-7b216ba653d8', 4);
